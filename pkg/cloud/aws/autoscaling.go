6a0d02ff6ce19c30965a59a207a11f434741cd71
AkiraFukushima <h3.poteto@gmail.com>
2021-01-21 18:26:54 +0900

    refs #5 Increase instances equally across all AutoScalingGroups

--- pkg/cloud/aws/autoscaling.go
+++ pkg/cloud/aws/autoscaling.go
@@ -2,7 +2,6 @@ package aws
 
 import (
 	"errors"
-	"fmt"
 	"sort"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -25,43 +24,46 @@ func (a *AWS) AddInstancesToAutoScalingGroups(groups []operatorv1alpha1.AutoScal
 		klog.Errorf("failed to describe AutoScalingGroups: %v", err)
 		return err
 	}
-	sumCurrentDesired := 0
-	var asgs []*autoscaling.Group
+
+	sumASGDesired := 0
 	// safetyASGs have same value desired capacity and current instances count.
 	var safetyASGs []*autoscaling.Group
 	for _, asg := range output.AutoScalingGroups {
-		sumCurrentDesired += int(*asg.DesiredCapacity)
-		asgs = append(asgs, asg)
+		sumASGDesired += int(*asg.DesiredCapacity)
 		if int(*asg.DesiredCapacity) == len(asg.Instances) {
 			safetyASGs = append(safetyASGs, asg)
 		}
 	}
-	sort.SliceStable(asgs, func(i, j int) bool {
-		return (*asgs[i].MaxSize - *asgs[i].DesiredCapacity) > (*asgs[j].MaxSize - *asgs[j].DesiredCapacity)
-	})
+
+	if len(safetyASGs) < 1 {
+		err := errors.New("there are no safety AutoScalingGroups, so could not add instances")
+		klog.Error(err)
+		return err
+	}
+
 	sort.SliceStable(safetyASGs, func(i, j int) bool {
 		return (*safetyASGs[i].MaxSize - *safetyASGs[i].DesiredCapacity) > (*safetyASGs[j].MaxSize - *safetyASGs[j].DesiredCapacity)
 	})
 
-	// Increment smallest desired ASG when spec desired and current desired are different.
-	if totalDesired != sumCurrentDesired {
-		// Smallest desired ASG
-		targetASG := asgs[0]
-		newDesired := int(*targetASG.DesiredCapacity) + count
-		klog.Infof("spec desired is %d, and current ASG desired is %d, so increment desired capacity of smallest ASG: %s", totalDesired, sumCurrentDesired, *targetASG.AutoScalingGroupName)
-		return updateASGCapacity(a.autoscaling, targetASG, newDesired)
-	} else {
-		// Add instance in safety ASG when spec desired and current desired are same.
-		if len(safetyASGs) < 1 {
-			err := errors.New("there are no safety AutoScalingGroups, so could not add instances")
-			klog.Error(err)
-			return err
+	// Increase desired capacity equally across all ASGs.
+	surplus := count
+	for surplus > 0 {
+		// Exit this loop when all ASGs capacity is fullfilled.
+		if fullfilled := allASGIsFullfilled(safetyASGs); fullfilled {
+			break
 		}
-		targetASG := safetyASGs[0]
-		newDesired := int(*targetASG.DesiredCapacity) + count
-		klog.Infof("spec desired is %d, and current ASG desired is %d, so increment desired capacity of safety ASG: %s", totalDesired, sumCurrentDesired, *targetASG.AutoScalingGroupName)
-		return updateASGCapacity(a.autoscaling, targetASG, newDesired)
+		for j := range safetyASGs {
+			if int(*safetyASGs[j].MaxSize-*safetyASGs[j].DesiredCapacity) > 0 {
+				*safetyASGs[j].DesiredCapacity += 1
+				surplus--
+			}
+		}
 	}
+	klog.Infof("spec desired is %d, and current ASGs desired is %d, so increase desired capacity for all ASGs", totalDesired, sumASGDesired)
+	if surplus > 0 {
+		klog.Warningf("all ASGs is already fullfilled so could nod add %d instances", surplus)
+	}
+	return updateASGsDesired(a.autoscaling, safetyASGs)
 }
 
 func (a *AWS) DeleteInstancesToAutoScalingGroups(groups []operatorv1alpha1.AutoScalingGroup, totalDesired int, count int) error {
@@ -116,24 +118,27 @@ func (a *AWS) DeleteInstancesToAutoScalingGroups(groups []operatorv1alpha1.AutoS
 	return updateASGCapacity(a.autoscaling, targetASG, newDesired)
 }
 
+func updateASGsDesired(client *autoscaling.AutoScaling, asgs []*autoscaling.Group) error {
+	var err []error
+	for i := range asgs {
+		if e := updateASGCapacity(client, asgs[i], int(*asgs[i].DesiredCapacity)); e != nil {
+			err = append(err, e)
+		}
+	}
+	if len(err) > 0 {
+		return err[0]
+	}
+	return nil
+}
+
 func updateASGCapacity(client *autoscaling.AutoScaling, asg *autoscaling.Group, newDesired int) error {
 	if newDesired > int(*asg.MaxSize) {
 		klog.Warningf("AutoScalingGroup %s has reached capacity limit, new desired: %d, but max: %d, so reduce new desired", *asg.AutoScalingGroupName, newDesired, *asg.MaxSize)
 		newDesired = int(*asg.MaxSize)
 	}
-	if newDesired == int(*asg.DesiredCapacity) {
-		err := fmt.Errorf("AutoScalingGroup %s has already fullfilled, so could not update desired capacity", *asg.AutoScalingGroupName)
-		klog.Error(err)
-		return err
-	}
 	if newDesired < int(*asg.MinSize) {
 		klog.Warningf("AutoScalingGroup %s has reached capacity minimize, new desired: %d, but min: %d so increase new desired", *asg.AutoScalingGroupName, newDesired, *asg.MinSize)
 		newDesired = int(*asg.MinSize)
-	}
-	if newDesired == int(*asg.DesiredCapacity) {
-		err := fmt.Errorf("AutoScalingGroup %s has already minimized, so could not update desired capacity", *asg.AutoScalingGroupName)
-		klog.Error(err)
-		return err
 	}
 	updateInput := &autoscaling.UpdateAutoScalingGroupInput{
 		AutoScalingGroupName: asg.AutoScalingGroupName,
@@ -146,4 +151,13 @@ func updateASGCapacity(client *autoscaling.AutoScaling, asg *autoscaling.Group, 
 	}
 	klog.Infof("updated desired capacity of AutoScalingGroup %s", *asg.AutoScalingGroupName)
 	return nil
+}
+
+func allASGIsFullfilled(asgs []*autoscaling.Group) bool {
+	for i := range asgs {
+		if int(*asgs[i].MaxSize-*asgs[i].DesiredCapacity) > 0 {
+			return false
+		}
+	}
+	return true
 }
